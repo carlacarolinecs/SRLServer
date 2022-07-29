b03b145e046b138cafbb2534de74482b95ea715c
Carla Caroline Carvalho Silva <77733177+carlacarolinecs@users.noreply.github.com>
2022-07-29 12:10:41 -0300

    Add files via upload
    Aula 4- Curso de SQL - https://www.youtube.com/watch?v=Yvw60m0E9D8&list=PL7iAT8C5wumpQWB8AFW7CwK2nlzh8ZdP9&index=5

---
+++ Scripts de criação de tabelas.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Alunos
+( 
+id_aluno int PRIMARY KEY NOT NULL,
+nome varchar(200) NOT NULL,
+data_nascimento date NOT NULL,
+sexo varchar(1) NOT NULL,  -- M para Masculino e F para Feminino
+data_cadastro datetime NOT NULL,
+login_cadastro varchar(15) NOT NULL
+);
+
+--DROP TABLE Alunos
+
+CREATE TABLE Situacao
+(
+id_situacao int PRIMARY KEY NOT NULL,
+situacao varchar(25) NOT NULL,
+data_cadastro datetime NOT NULL,
+login_cadastro varchar(15) NOT NULL
+);
+
+CREATE TABLE Cursos
+(
+id_curso int PRIMARY KEY NOT NULL,
+curso varchar(200) NOT NULL,
+data_cadastro datetime NOT NULL,
+login_cadastro varchar(15) NOT NULL
+);
+
+CREATE TABLE Turmas 
+(
+id_turma int PRIMARY KEY NOT NULL,
+id_aluno int NOT NULL,
+id_curso int NOT NULL,
+valor_turma numeric(15,2) NOT NULL,
+desconto numeric(3,2) NOT NULL,
+data_inicio date NOT NULL,
+data_termino date,
+data_cadastro datetime NOT NULL,
+login_cadastro varchar(15) NOT NULL
+);
+
+CREATE TABLE Registro_presenca
+(
+id_turma int NOT NULL,
+id_aluno int NOT NULL,
+id_situacao int NOT NULL,
+data_presenca date NOT NULL,
+data_cadastro datetime NOT NULL,
+login_cadastro varchar(15) NOT NULL
+);
+
+/*
+DROP TABLE Alunos;
+DROP TABLE Cursos;
+DROP TABLE Situacao;
+DROP TABLE Turmas;
+DROP TABLE Registro_presenca
+*/
